6e0f34ab1a31ec7f1184762be733a4a92c433509
sanchoo1 <sanchowong1@gmail.com>
2025-05-08 23:44:26 -0400

    0000

--- model_pipeline/appp.py
+++ model_pipeline/appp.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "app:app",      # 文件名:FastAPI 实例名
-        host="129.114.108.145", # 让外部可访问
+        host="0.0.0.0", # 让外部可访问
         port=5000,      # 你指定的 5000 端口
         reload=True     # 开发时自动热重载
     )
